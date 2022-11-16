ba186da8bcfbf4bbbc0cf1a9910d2b3633461452
aguskrmn <acarman@udesa.edu.ar>
2022-11-16 18:35:52 -0300

    fav

--- js/favoritos.js
+++ js/favoritos.js
@@ -5,3 +5,23 @@ let favoritos = JSON.parse(RecuperoStorage)
 // lo convierto en un dato manipulable para jv (array) //
 
 let section = document.querySelector(".sectoin")
+
+let PersonajeFavoritos = ""
+for (let i = 0; i < favoritos.length; i++) {
+    let url = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US"
+    fetch(Url)
+
+    .then(function (response) {
+    return response.json()
+    })
+
+    .then(function (data) {
+   
+    return data   
+    }   
+    )
+
+    .catch(function (error) {
+    return error
+    })
+    }
