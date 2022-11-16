0781b88a3c19aff356df8782a8138a226582c8f4
esuarez2004 <esuarez@udesa.edu.ar>
2022-11-15 22:41:12 -0300

    js

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// peliculas populares
+// fetch(``) // Api donde se intercambia info. entre aplicacions, mediante archivos de texto
+//     .then(function(response){
+//         return response.json(); // Transforma en una cadena de texto para que intercambie los datos 
+//     })  // Se utiliza los dos parentes, luego del JSON porque es un metodo
+//     .then(function(datos){
+
+//         console.log(datos.results);
+
+//         for(let i = 0; i < 5; i++){ // Se utiliza un bucle for para que no haga falta repetir el proceso 
+//             document.querySelector('.peliculasPopulares').innerHTML += `
+//             <article>
+//             </article>
+//             `;
+//         };
+//     })
+//     .catch(function(error){
+//         alert("El error es: " + error);
+//     })
+
+// Series mas vistas
+
+// Peliculas mas vistas
+
+
