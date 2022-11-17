a46866ab13164757a66ee6d5a7a33c0d0e68de00
aguskrmn <acarman@udesa.edu.ar>
2022-11-17 11:39:03 -0300

    mejoras favoritos

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,14 +1,24 @@
 let RecuperoStorage = localStorage.getItem("favoritos");    
-// llega en un json/ 
+// llega en un json/ y porque en apllication no esta favoritos
 
 let favoritos = JSON.parse(RecuperoStorage)
 // lo convierto en un dato manipulable para jv (array) //
 
 let section = document.querySelector(".sectoin")
+// alert (section)   porque no me dice object html element
 
-let PersonajeFavoritos = ""
-for (let i = 0; i < favoritos.length; i++) {
+let PersonajeFavoritos = '';
+
+if (favoritos == null || favoritos.length == 0) {
+/*no hay favoritos*/
+section.innerHTML = '<p> NO hayb datos en favoritos<p/>'
+} 
+
+else {
+    for (let i = 0; i < favoritos.length; i++) {
     let url = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US"
+    /* url de detalle   $favoritos[i]*/
+    
     fetch(url)
 
     .then(function (response) {
@@ -16,12 +26,15 @@ for (let i = 0; i < favoritos.length; i++) {
     })
 
     .then(function (data) {
-   
+    console.log(data);
     return data   
     }   
     )
 
     .catch(function (error) {
+    console.log(error);
     return error
     })
     }
+    
+}
