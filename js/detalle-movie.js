cac251f91766aff712bf379396832e46910a87f6
JuanGomezD~ <jgomezdelgado@udesa.edu.ar>
2022-11-18 10:33:43 -0300

    carman subiendo

--- js/detalle-movie.js
+++ js/detalle-movie.js
@@ -1,14 +1,18 @@
 let favorito = document.querySelector(".clicFav")
 let qs = location.search
 let qsObj = new URLSearchParams(qs);
-let id = qsObj.get("buscador");
+let id = qsObj.get("id");
 
-const url = `https://api.themoviedb.org/3/movie/${id}?api_key=%3C%3Capi_key%3E%3E&language=en-US`
 
-fetch(url).then(function (response) {
+let apiKey = "f2acabc2f1f7dfa29f6493c2fcca003f"
+const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=es`
+
+fetch(url)
+
+.then(function(response) {
     return response.json();
 }).then(function (data) {
-    console.log(data.response);
+    console.log(data);
 }).catch(function (error) {
     return error;
 })
@@ -21,17 +25,17 @@ if (recuperoStorage != null) {
     favoritos = JSON.parse(recuperoStorage)
 }
 if (favoritos.includes(id)) {
-    fav.innerText = "Quitar de Favoritos";
+    clicFav.innerText = "Quitar de Favoritos";
 }
-fav.addEventListener("click",function(e) {
+clicFav.addEventListener("click",function(e) {
     e.preventDefault();
 
     if (favoritos.includes(id)){
         let indice = favoritos.indexOf(id)
         favoritos.splice(indice, 1);
-        fav.innerText = "Agregar a Favoritos"
+        clicFav.innerText = "Agregar a Favoritos"
     }else{
         favoritos.push(id)
-        fav.innerText = "Quitar de favoritos"
+        clicFav.innerText = "Quitar de favoritos"
     }
 })
