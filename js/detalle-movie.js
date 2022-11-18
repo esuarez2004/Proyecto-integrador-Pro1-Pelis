1febdaf340b0b2ee769dd0f507ee0ea68075b2f5
JuanGomezD~ <jgomezdelgado@udesa.edu.ar>
2022-11-18 09:32:07 -0300

    ax2

--- js/detalle-movie.js
+++ js/detalle-movie.js
@@ -1,1 +1,14 @@
-fetch(url)
+let favorito = document.querySelector(".clicFav")
+let qs = location.search
+let qsObj = new URLSearchParams(qs);
+let id = qsObj.get("buscador");
+
+const url = `https://api.themoviedb.org/3/movie/${id}?api_key=%3C%3Capi_key%3E%3E&language=en-US`
+
+fetch(url).then(function (response) {
+    return response.json();
+}).then(function (data) {
+    console.log(data.response);
+}).catch(function (error) {
+    return error;
+})
