15be0c3bf69e4f06be45fa54264efb2b4b8915c8
aguskrmn <acarman@udesa.edu.ar>
2022-11-18 11:05:26 -0300

    sub

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,8 +1,8 @@
 let RecuperoStorage = localStorage.getItem("favoritos");    
-// llega en un json/ 
+/* llega en un json */ 
 
 let favoritos = JSON.parse(RecuperoStorage)
-// lo convierto en un dato manipulable para jv (array) //
+/* lo convierto en un dato manipulable para jv (array) */
 
 let section = document.querySelector(".section")
 
