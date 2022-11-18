30743409e711a599a1a84aa481847ece854b17c5
esuarez2004 <esuarez@udesa.edu.ar>
2022-11-18 10:32:53 -0300

    index finish

--- js/index.js
+++ js/index.js
@@ -7,15 +7,15 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f2acabc2f1f7dfa29f6493
         .then(function(datos){
              console.log(datos.results);
 
-            for (let i = 0; i < datos.results.length; i++) {
+            for (let i = 0; i < 4; i++) {
                 Contenedor.innerHTML += 
-                `<article>
+                `<article ">
                     <div>
                         <img src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="pelicula">
                     </div>
                     <h5>${datos.results[i].title}</h5>
                     <p>${datos.results[i].release_date}</p>
-                    <a href="./detail_movie.html"?id=${datos.results[i].id}">Ver mas informacion</a>
+                    <a href="./detail_movie.html?id=${datos.results[i].id}">Ver mas informacion</a>
                 </article>`
                 
             }
@@ -25,7 +25,52 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f2acabc2f1f7dfa29f6493
         })
 
 // Series mas vistas
+let contenedor2= document.querySelector(".SeriesMasVistas")
+fetch(`https://api.themoviedb.org/3/tv/popular?api_key=f2acabc2f1f7dfa29f6493c2fcca003f`) // Api donde se intercambia info. entre aplicacions, mediante archivos de texto
+       .then(function(response){
+           return response.json(); // Transforma en una cadena de texto para que intercambie los datos 
+})  // Se utiliza los dos parentes, luego del JSON porque es un metodo
+        .then(function(datos){
+             console.log(datos.results);
 
+            for (let i = 0; i < 4; i++) {
+                contenedor2.innerHTML += 
+                `<article ">
+                    <div>
+                        <img src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="pelicula">
+                    </div>
+                    <h5>${datos.results[i].title}</h5>
+                    <p>${datos.results[i].release_date}</p>
+                    <a href="./detail_movie.html?id=${datos.results[i].id}">Ver mas informacion</a>
+                </article>`
+                
+            }
+            })
+        .catch(function (error) {
+            return error
+        })
 // Peliculas mas vistas
+let contenedor3 = document.querySelector(".PeliculasMasVistas")
+fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=f2acabc2f1f7dfa29f6493c2fcca003f`)
+.then(function(response){
+    return response.json(); // Transforma en una cadena de texto para que intercambie los datos 
+})  // Se utiliza los dos parentes, luego del JSON porque es un metodo
+ .then(function(datos){
+      console.log(datos.results);
 
-
+     for (let i = 0; i < 4; i++) {
+         contenedor3.innerHTML += 
+         `<article ">
+             <div>
+                 <img src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="pelicula">
+             </div>
+             <h5>${datos.results[i].title}</h5>
+             <p>${datos.results[i].release_date}</p>
+             <a href="./detail_movie.html?id=${datos.results[i].id}">Ver mas informacion</a>
+         </article>`
+         
+     }
+     })
+ .catch(function (error) {
+     return error
+ })
