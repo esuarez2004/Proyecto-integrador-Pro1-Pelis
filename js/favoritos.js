5577ca7ebb9dc17060762d768ce3b94a4339a7c4
aguskrmn <acarman@udesa.edu.ar>
2022-11-22 14:25:58 -0300

    favoritos para serie

--- js/favoritos.js
+++ js/favoritos.js
@@ -50,3 +50,53 @@ else {
 }
 
 /* Lo mismo con serie */
+let RecuperoStoragex = localStorage.getItem("favoritos");    
+/* llega en un json */ 
+
+let favoritosx = JSON.parse(RecuperoStorage)
+/* lo convierto en un dato manipulable para jv (array) */
+
+let sectionx = document.querySelector(".section")
+
+let apiKeyx = "f2acabc2f1f7dfa29f6493c2fcca003f"
+
+let PersonajeFavoritosx = '';
+
+if (favoritosx == null || favoritosx.length == 0) {
+/*si no hay favoritos*/
+section.innerHTML = '<strong class="NoFav"> NO hay series ni peliculas en favoritos</strong>'
+} 
+
+else {
+    for (let i = 0; i < favoritosx.length; i++) {
+    let url = `https://api.themoviedb.org/3/tv/${favoritosx[i]}?api_key=${apiKey}&language=es`
+    
+    fetch(url)
+
+    .then(function (response) {
+    return response.json()
+    })
+
+    .then(function (data) {
+    console.log(data);
+        PersonajeFavoritosx += `<article>
+                                <div>
+                                <a href="./detalle_serie.html?id=${data.id}"><img src="https://image.tmdb.org/t/p/w342${data.poster_path}" alt="${data.original_title}"></a> 
+                                </div>
+                                <h5 class="Titulo-search">Titulo: ${data.original_title}</h5>
+                                <a href="./detalle_serie.html?id=${data.id}">Ver mas informacion</a>
+                              </article>`
+        
+    section.innerHTML = PersonajeFavoritos
+    /* para cuando termine el for se presentren en el DOM*/
+    return data   
+    }   
+    )
+
+    .catch(function (error) {
+    console.log(error);
+    return error
+    })
+    }
+    
+}
